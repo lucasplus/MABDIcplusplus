12a9b0beaf265bc0d2ddff3376fb38a2684966f9
Lucas Chavez <golucasplus@gmail.com>
2014-09-22 18:42:19 -0600

    Happier with vtk zbuffer test and more general Cmake
    test_vtk_zbuffer is proving the concept better now
    
    CMakeLists.txt no longer needs to specifically say where qt and vtk are.
    I set QTDIR and VTKDIR in my environmental variables and now it just
    finds them!

--- tests/test_vtk_zbuffer/main.cpp
+++ tests/test_vtk_zbuffer/main.cpp
@@ -9,40 +9,99 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTerrain.h>
 
 #include <vtkWindowToImageFilter.h>
-#include <vtkImageShiftScale.h>
 
 #include <vtkImageMapper.h>
 #include <vtkActor2D.h>
 #include <vtkInteractorStyleImage.h>
 
+#include <vtkCallbackCommand.h>
+
+#include <vtkCamera.h>
+
 #include <string>
 #include <vector>
 
 #include "MABDI_Util.h"
- 
+
+
+// The callback does the work.
+// The callback keeps a pointer to the sphere whose resolution is
+// controlled. After constructing the callback, the program sets the
+// SphereSource of the callback to
+// the object to be controlled.
+class MyCallbackClass : public vtkCommand
+{
+public:
+  static MyCallbackClass *New()
+    {
+    return new MyCallbackClass;
+    }
+  virtual void Execute(vtkObject *caller, unsigned long, void*)
+    {
+      double camPosition[3];
+      double clipRange[2];
+      double viewAngle;
+
+      this->FilterSource->Modified();
+      this->RenderWindowSource->Render();
+      
+      std::cout << "rendered event " << std::endl;
+      
+      this->RenderWindowCamera->GetPosition( camPosition );
+      std::cout << "cam position " 
+        << camPosition[0] << " " << camPosition[1] << " " << camPosition[2] << std::endl;
+      
+      this->RenderWindowCamera->GetClippingRange( clipRange );
+      viewAngle = this->RenderWindowCamera->GetViewAngle();
+
+      std::cout << " cam intrinsics " 
+        << " np: " << clipRange[0] << " fp: " << clipRange[1] << " fovy: " << viewAngle << std::endl;
+
+    }
+  MyCallbackClass(): 
+    FilterSource(0), 
+    RenderWindowSource(0), 
+    RenderWindowCamera(0) {}
+  vtkWindowToImageFilter *FilterSource;
+  vtkRenderWindow *RenderWindowSource;
+  vtkCamera *RenderWindowCamera;
+};
+
 int main(int argc, char *argv[])
 {
 
-  // init vtk classes
-  UTIL_VTK_INIT( vtkSTLReader             , fileReader       );
-  UTIL_VTK_INIT( vtkPolyDataMapper        , mapper           );
-  UTIL_VTK_INIT( vtkActor                 , actor            );
-  UTIL_VTK_INIT( vtkRenderer              , renderer         );
-  UTIL_VTK_INIT( vtkRenderWindow          , renderWindow     );
-  UTIL_VTK_INIT( vtkRenderWindowInteractor, interactor       );
-  UTIL_VTK_INIT( vtkWindowToImageFilter   , filter           );
-  UTIL_VTK_INIT( vtkImageMapper           , imageMapper      );
-  UTIL_VTK_INIT( vtkActor2D               , imageActor       );
-  UTIL_VTK_INIT( vtkRenderWindow          , renderWindowImage);
-  UTIL_VTK_INIT( vtkRenderer              , rendererImage    );
+  UTIL_VTK_INIT( vtkSTLReader               , fileReader            );
+  UTIL_VTK_INIT( vtkPolyDataMapper          , mapper                );
+  UTIL_VTK_INIT( vtkActor                   , actor                 );
+  UTIL_VTK_INIT( vtkRenderer                , renderer              );
+  UTIL_VTK_INIT( vtkRenderWindow            , renderWindow          );
+  UTIL_VTK_INIT( vtkRenderWindowInteractor  , interactor            );
+  UTIL_VTK_INIT( vtkInteractorStyleTerrain  , styleTerrain          );
+
+  //UTIL_VTK_INIT( vtkCallbackCommand         , callbackCommand       );
+
+  UTIL_VTK_INIT( vtkWindowToImageFilter     , filter                );
+  UTIL_VTK_INIT( vtkImageMapper             , imageMapper           );
+  UTIL_VTK_INIT( vtkActor2D                 , imageActor            );
+  UTIL_VTK_INIT( vtkRenderWindow            , renderWindowImage     );
+  UTIL_VTK_INIT( vtkRenderer                , rendererImage         );
+  UTIL_VTK_INIT( MyCallbackClass            , myCallback            );
+
+  UTIL_VTK_INIT( vtkCamera                  , ActiveCamera          );
 
   // window and renderer set-up
   renderWindow->AddRenderer( renderer );
   renderWindowImage->AddRenderer( rendererImage );
   interactor->SetRenderWindow( renderWindow );
   
+  renderWindow->SetSize( 640, 480 );
+  renderWindowImage->SetSize( 640, 480 );
+
+  //renderWindow->SetOffScreenRendering( 1 );
+
   // where is the file to read in?
   std::string tableFilePath( MABDI_Util::StlEnvironmentDir ); 
   tableFilePath.append( "table.stl" );
@@ -63,119 +122,38 @@ int main(int argc, char *argv[])
   renderer->SetBackground( rgb[0], rgb[1], rgb[2] );
 
   renderWindow->Render();
-  interactor->Start();
 
   filter->SetInput( renderWindow );
-  filter->SetInput(renderWindow);
-  filter->SetMagnification(2);
-  filter->Update();
+  filter->SetInputBufferTypeToZBuffer();
 
-  // copy the output
-  vtkImageData * outputData = filter->GetOutput()->NewInstance();
-  outputData->DeepCopy(filter->GetOutput());
-
-  imageMapper->SetInputData(outputData);
-  imageMapper->SetColorWindow(255);
-  imageMapper->SetColorLevel(127.5);
+  imageMapper->SetInputConnection( filter->GetOutputPort() );
+  imageMapper->SetColorWindow( 1 );
+  imageMapper->SetColorLevel( 0.5 );
 
   imageActor->SetMapper( imageMapper );
 
-  //renWin->SetSize(320, 320);
-  //renWin->SetPosition(320,320);
+  rendererImage->AddActor( imageActor );
 
-  rendererImage->AddActor(imageActor);
+  ActiveCamera = renderer->GetActiveCamera();
 
-  renderWindow->Render();
-  renderWindowImage->Render();
+  // set up callback for when the renderwindow renders
+  myCallback->FilterSource = filter;
+  myCallback->RenderWindowSource = renderWindowImage;
+  myCallback->RenderWindowCamera = ActiveCamera;
+  renderWindow->AddObserver( vtkCommand::RenderEvent, myCallback );
 
+  styleTerrain->SetAutoAdjustCameraClippingRange( 0 );
+
+  interactor->SetInteractorStyle( styleTerrain );
+
+  ActiveCamera->SetViewAngle( 60.0 );
+  ActiveCamera->SetClippingRange( 0.5, 10.0 );
+
+  int isParallelProjection = ActiveCamera->GetParallelProjection();
+  std::cout << " cam parallel projection " << isParallelProjection << std::endl;
+
+  interactor->Start();
+  
   return EXIT_SUCCESS;
 }
 
-/*
-// initialize vtk classes
-
-  UTIL_VTK_INIT( vtkSTLReader             , fileReader       );
-  UTIL_VTK_INIT( vtkPolyDataMapper        , mapper           );
-  UTIL_VTK_INIT( vtkActor                 , actor            );
-  UTIL_VTK_INIT( vtkRenderer              , rendererPoly     );
-  UTIL_VTK_INIT( vtkRenderer              , rendererImage    );
-  UTIL_VTK_INIT( vtkRenderWindow          , renWinPoly       );
-  UTIL_VTK_INIT( vtkRenderWindow          , renWinImage      );
-
-  UTIL_VTK_INIT( vtkRenderWindowInteractor, interactorPoly   );
-  UTIL_VTK_INIT( vtkRenderWindowInteractor, interactorImage  );
-  UTIL_VTK_INIT( vtkInteractorStyleImage  , style            );
-  
-  UTIL_VTK_INIT( vtkWindowToImageFilter   , filter           );
-  UTIL_VTK_INIT( vtkImageMapper           , imageMapper      );
-  UTIL_VTK_INIT( vtkImageShiftScale       , scale            );
-  
-
-
-  
-  
-  interactorImage->SetInteractorStyle( style );
-
-  
-
-  //vtkSmartPointer<vtkImageShiftScale> scale =
-  //  vtkSmartPointer<vtkImageShiftScale>::New();
- 
-  // read .ply file
-  fileReader->SetFileName("util/stl/environment/table.stl");
- 
-  // Build visualization enviroment
-  mapper->SetInputConnection( fileReader->GetOutputPort() );
-  actor->SetMapper(mapper);
-  rendererPoly->AddActor(actor);
-  renWinPoly->AddRenderer(rendererPoly);
-  interactorPoly->SetRenderWindow(renWinPoly);
-  renWinPoly->Render();
-  interactorPoly->Start();
-  
-  // Create Depth Map
-  filter->SetInput(renWinPoly);
-  filter->SetMagnification(1);
-  filter->SetInputBufferTypeToZBuffer();  // extract z buffer value
-  
-  
-
-  scale->SetOutputScalarTypeToUnsignedChar();
-  scale->SetInputConnection( filter->GetOutputPort() );
-  scale->SetShift(0);
-  scale->SetScale(-255);
-
-  imageMapper->SetInputData( scale->GetOutput() );
-
-  vtkSmartPointer<vtkActor2D> imageActor = 
-    vtkSmartPointer<vtkActor2D>::New();
-
-  imageActor->SetMapper( imageMapper );
-
-  renWinImage->AddRenderer( rendererImage );
-
-  
-
-  interactorImage->SetRenderWindow( renWinImage );
-  
-  rendererImage->AddActor2D( imageActor );
-  
-  renWinImage->Render();
-  interactorImage->Start();
-
-  vtkImageData* myImage = filter->GetOutput();
-  
-  
-  //scale->SetOutputScalarTypeToUnsignedChar();
-  //scale->SetInputConnection(filter->GetOutputPort());
-  //scale->SetShift(0);
-  //scale->SetScale(-255);
-  
-  filter->GetOutputPort()
-
-  // Write depth map as a .bmp image
-  imageWriter->SetFileName("hai.bmp");
-  imageWriter->SetInputConnection(scale->GetOutputPort());
-  imageWriter->Write();
-  
-*/
